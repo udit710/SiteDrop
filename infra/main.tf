c8bb91dc555cf6509b06bc707b75f59cc5f7bb83
udit710 <udit.malshe@iress.com>
2025-09-20 10:19:00 +1000

    change tf

--- infra/main.tf
+++ infra/main.tf
@@ -52,7 +52,16 @@ resource "aws_cloudfront_distribution" "cdn" {
     viewer_protocol_policy = "redirect-to-https"
     allowed_methods        = ["GET", "HEAD"]
     cached_methods         = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
   }
+
 
   price_class = "PriceClass_100" # cheapest option (US, Canada, Europe only)
 
